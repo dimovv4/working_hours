8645038def143e8b834e90cd7dae7c60e168df7d
Georgi Dimov <147717567+dimovv4@users.noreply.github.com>
2023-11-12 20:28:25 +0200

    Add files via upload

---
+++ working_hours.py
@@ -0,0 +1,25 @@
+hour = int(input())
+day_of_week = input()
+user_output = ''
+
+if day_of_week == "Sunday":
+    user_output = 'closed'
+else:
+    if hour >= 10 and hour <= 18:
+        user_output = 'open'
+    else:
+        user_output = 'closed'
+
+print(user_output)
+
+
+
+# time = int(input())
+# day_of_the_week = input().lower()
+#
+# if day_of_the_week == "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday" or "Saturday":
+#     if 10 <= time <= 18:
+#         print("open")
+#     else:
+#         print("closed")
+#
